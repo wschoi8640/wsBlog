683d75405838b22a6b17ef202f22a4f359875c1a
wschoi8640 <cws911112@gmail.com>
2019-05-23 23:08:10 +0900

    UPDATE Modifications (Modulize login Service and Controller)

--- src/main/java/com/wschoi/wsblog/controller/MemberController.java
+++ src/main/java/com/wschoi/wsblog/controller/MemberController.java
@@ -1,8 +1,38 @@
 package com.wschoi.wsblog.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.wschoi.wsblog.service.LoginService;
 
 @Controller
-public class MemberController {
+public class MemberController 
+{
+		private static final Logger logPrinter = LoggerFactory.getLogger(MemberController.class);
 
+		@Autowired
+		LoginService loginService;
+		
+		@RequestMapping(value = "/login", method = RequestMethod.POST)
+		public String redirectToIndex(Model model,
+				HttpServletResponse response,
+				@RequestParam("userID") String userID, 
+				@RequestParam("userPassword") String userPW) throws ServletException, IOException
+		{
+				logPrinter.info("checking Login Data");
+				loginService.login(model, response, userID, userPW);
+				
+				return "";
+		}
 }
---
+++ src/main/java/com/wschoi/wsblog/service/LoginService.java
@@ -0,0 +1,56 @@
+package com.wschoi.wsblog.service;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.wschoi.wsblog.dao.UserDAO;
+
+@Service
+public class LoginService 
+{
+		private static final Logger logPrinter = LoggerFactory.getLogger(LoginService.class);
+	
+		@Autowired
+		UserDAO userDAO;
+		
+		public int login(Model model,
+				HttpServletResponse response, String encodedUserID, String encodedUserPW) throws ServletException, IOException 
+		{
+				
+				String userID = URLDecoder.decode(encodedUserID, "UTF-8");
+				String userPW = URLDecoder.decode(encodedUserPW, "UTF-8");
+				int result = userDAO.login(userID, userPW);
+				
+				if(result == 1) 
+				{	
+					logPrinter.info("Login Successful");
+					model.addAttribute("userID", userID);
+					response.getWriter().write("1"); //login suc
+				}
+				if(result == 0)
+				{
+					logPrinter.info("Login Failed - wrong password");
+					response.getWriter().write("0"); //wrong pw
+				}
+				if(result == -1)
+				{
+					logPrinter.info("Login Failed - no such ID");
+					response.getWriter().write("-1"); //no such ID
+				}
+				if(result == -2)
+				{
+					logPrinter.info("Login Failed - DB error");
+					response.getWriter().write("-2"); // db Error
+				}
+				return result;
+		}
+}
